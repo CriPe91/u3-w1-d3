8077dc39c662a0e08c596da785785e6ac4f1977b
CriPe91 <cristian.petta91@gmail.com>
2024-11-27 19:31:29 +0100

    u3w1d3React

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,16 +1,35 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, FormControl } from "react-bootstrap";
 import SingleBook from "./SingleBook";
+import { Component } from "react";
 
-const BookList = (props) => {
-  return (
-    <Row className="gy-3">
-      {props.books.map((book) => (
-        <Col key={book.asin} md={6} lg={4}>
-          <SingleBook book={book} />
-        </Col>
-      ))}
-    </Row>
-  );
-};
-
+class BookList extends Component {
+  state = {
+    FiltraLibri: "",
+  };
+  render() {
+    return (
+      <>
+        <Row className="justify-content-center mb-3">
+          <Col xs={6}>
+            <FormControl
+              type="text"
+              placeholder="Filtra i libri"
+              value={this.state.FiltraLibri}
+              onChange={(e) => this.setState({ FiltraLibri: e.target.value })}
+            />
+          </Col>
+        </Row>
+        <Row className="gy-3">
+          {this.props.books
+            .filter((book) => book.title.toLowerCase().includes(this.state.FiltraLibri.toLowerCase()))
+            .map((book) => (
+              <Col key={book.asin} md={6} lg={3}>
+                <SingleBook book={book} />
+              </Col>
+            ))}
+        </Row>
+      </>
+    );
+  }
+}
 export default BookList;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,17 +1,26 @@
+import { Component } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 
-const SingleBook = (props) => (
-  <Container>
-    <Row className="justify-content-center">
-      <Col>
-        <Card bg="light">
-          <Card.Img variant="top" src={props.book.img} />
-          <Card.Body>
-            <Card.Title>{props.book.title}</Card.Title>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  </Container>
-);
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center">
+          <Col>
+            <Card className={this.state.selected ? "border-danger" : "border-light"}>
+              <Card.Img variant="top" src={this.props.book.img} onClick={() => this.setState({ selected: !this.state.selected })} />
+              <Card.Body>
+                <Card.Title>{this.props.book.title}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
 export default SingleBook;
